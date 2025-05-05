86211c939440301d115dda621a64ec81e3807461
bPratik603 <bhosalepratik93@gmail.com>
2025-05-05 21:54:15 +0530

    Factorial using static function

--- 05_05_2025/Factorial.java
+++ 05_05_2025/Factorial.java
@@ -1,16 +1,20 @@
 class Factorial{
     public static void main(String[] args){
-
-        int count = 0;
-        int num = 15;
-
-         for(int i=1;i<=num;i++){
-            if(num % i == 0){
-                count++;
-                System.out.println(i);
-            }
-         }
-
+       int count = 0;
+       
+       count = fact(15);
+        
          System.out.println("Total Number of Factorial  = "+count);
     }
+
+  static int fact(int num){
+  int count = 0;
+    for(int i=1;i<=num;i++){
+        if(num % i == 0){
+            count++;
+        }
+     }
+     return count;
+  }
 }
+
