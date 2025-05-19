433c1d6a8bcf7cad9088b0cc5284c1d82df267ca
bPratik603 <bhosalepratik93@gmail.com>
2025-05-19 10:21:15 +0530

    sub Array

--- 3_Array/4_Sub_Array/Prog26.java
+++ 3_Array/4_Sub_Array/Prog26.java
@@ -3,8 +3,26 @@
 
     public static void main(String[] args) {
         
-        int arr[] = new int[]{1,2,3,1,2,4,6,4,3};
+        int arr[] = new int[]{2,4,1,3};
 
-        
+        int tmp[] = new int[arr.length];
+
+        tmp[0] = arr[0];
+        for(int i=1;i<arr.length;i++){
+            tmp[i] = tmp[i-1]+arr[i];
+        }
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+              int sum = 0;
+                if(i == 0){
+                    sum= tmp[j];
+                }else{
+                    sum = tmp[j] - tmp[i-1];
+                }
+
+                System.out.println(sum);
+            }
+        }
     }
 }
---
+++ 3_Array/4_Sub_Array/Prog27.java
@@ -0,0 +1,20 @@
+ class Prog27 {
+
+    public static void main(String[] args) {
+        
+        int arr[] = new int[]{1,2,3};
+
+        int totalSum = 0;
+
+        for(int i=0;i<arr.length;i++){
+            int sum =0;
+            for(int j=i;j<arr.length;j++){
+                sum = sum+arr[j];
+                totalSum = totalSum+sum;
+
+            }
+        }
+
+        System.out.println(totalSum);
+    }
+}
